ae1261422835a3f03be5689a4c8b340f8c79def8
chopliszt <juan.infante@perspektiva360.com>
2025-10-19 20:11:26 -0600

    add template base para herencia, que incluye el navbar y footer. También se agregó el primer modelo de estudiantes y se modificaton las views de la app cursos y las urls del proyecto general

--- proyecto_aprendizaje/cursos/models.py
+++ proyecto_aprendizaje/cursos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=150, null=False, blank=True)
+    pass
--- proyecto_aprendizaje/cursos/views.py
+++ proyecto_aprendizaje/cursos/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render  # get_object_or_404
+
+from .models import Estudiante
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, "cursos/index.html")
+    pass
+
+
+def lista_estudiantes(request):
+    estudiantes = Estudiante.objects.all()
+    return render(request, "", {"estudiantes": estudiantes})
