3e24bd07bca8827e5681397afc10165a416f196b
chopliszt <juan.infante@perspektiva360.com>
2025-10-19 20:20:47 -0600

    update el models.py para que tenga los otros dos modelos y el modelo Estudiante está más completo ahora

--- proyecto_aprendizaje/cursos/models.py
+++ proyecto_aprendizaje/cursos/models.py
@@ -3,6 +3,19 @@ from django.db import models
 # Create your models here.
 
 
+class Curso(models.Model):
+    pass
+
+
+class Plan(models.Model):
+    pass
+
+
 class Estudiante(models.Model):
     nombre = models.CharField(max_length=150, null=False, blank=True)
-    pass
+    email = models.EmailField(
+        max_length=254, unique=True, null=False, blank=False
+    )  # unique sirve para que en la DB el registro se unico
+
+    def __str__(self):
+        return f"{self.nombre}"
